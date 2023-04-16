7d68c59cd89e352dc8f54e71e9927c0b8eb40465
Neo Zhang <zhangz73@cs.washington.edu>
2023-04-16 00:45:43 -0400

    fix bugs in charging payoff

--- Utils/mdp.py
+++ Utils/mdp.py
@@ -297,7 +297,7 @@ class Reward:
             payoff = 0
         ## For now, charging rate assumes to be 0
 #        pay_rate = 0
-        return payoff #-0.5 #-pay_rate * rate
+        return -payoff #-0.5 #-pay_rate * rate
 
 ### This module implements the MDP process that does not allow interruptions of actions
 class MarkovDecisionProcess:
