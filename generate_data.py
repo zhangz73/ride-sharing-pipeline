10c6474850342f3897a8fc1911b34feb60e58b52
Neo Zhang <zhangz73@cs.washington.edu>
2023-06-15 16:36:18 -0700

    revert to previous rate and pack size

--- generate_data.py
+++ generate_data.py
@@ -187,8 +187,8 @@ def generate_spatialtemporal_star_to_complete_network(num_regions = 4, xi = 0.5)
 num_regions = 4
 num_cars = 12
 time_horizon = 48
-rate = 36
-pack_size = 36
+rate = 24#36
+pack_size = 24#36
 #num_chargers = 0 #48
 for num_chargers in [0, 48]:
     for xi in [0, 0.5, 1]:
