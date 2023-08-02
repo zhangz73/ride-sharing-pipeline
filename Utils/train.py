b68a9336ddb2fd809af8eda5bb3d1c33dbd79931
Neo Zhang <zhangz73@cs.washington.edu>
2023-08-02 13:37:55 -0700

    fixed bug in normalization across days

--- Utils/train.py
+++ Utils/train.py
@@ -133,6 +133,8 @@ class PPO_Solver(Solver):
         else:
             #next_value = 0
             next_value = self.value_model((0, next)).reshape((-1,))
+            mu2, sd2 = self.value_scale[0]
+            next_value = next_value * sd2 + mu2
         return (payoff + next_value * self.gamma - curr_value) / sd
     
     def get_ratio(self, state_counts, action_id, ts, clipped = False, eps = 0.2, car_id = None):
